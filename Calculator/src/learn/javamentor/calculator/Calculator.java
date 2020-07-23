4535d27839fd930a61a639ccda92f75b2c2cc9fa
Didar Akulov <tam-tam@bk.ru>
2020-07-23 17:12:23 +0600

    output roman numbers

--- Calculator/src/learn/javamentor/calculator/Calculator.java
+++ Calculator/src/learn/javamentor/calculator/Calculator.java
@@ -1,9 +1,54 @@
 package learn.javamentor.calculator;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
 
+	
+	public static String IntegerToRomanNumeral(int input) {
+	    if (input < 1 || input > 100)
+	        return "Invalid Roman Number Value";
+	    String s = "";
+	    while (input >= 100) {
+	        s += "C";
+	        input -= 100;
+	    }
+	    while (input >= 90) {
+	        s += "XC";
+	        input -= 90;
+	    }
+	    while (input >= 50) {
+	        s += "L";
+	        input -= 50;
+	    }
+	    while (input >= 40) {
+	        s += "XL";
+	        input -= 40;
+	    }
+	    while (input >= 10) {
+	        s += "X";
+	        input -= 10;
+	    }
+	    while (input >= 9) {
+	        s += "IX";
+	        input -= 9;
+	    }
+	    while (input >= 5) {
+	        s += "V";
+	        input -= 5;
+	    }
+	    while (input >= 4) {
+	        s += "IV";
+	        input -= 4;
+	    }
+	    while (input >= 1) {
+	        s += "I";
+	        input -= 1;
+	    }    
+	    return s;
+	}
+	
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
@@ -13,7 +58,8 @@ public class Calculator {
 		String[] twoNumbers = toCalculate.split("[+-/*]");
 		int num1 = 0;
 		int num2 = 0;
-		
+		int num3 = 0;
+		int num4 = 0;
 		String strNum1 = twoNumbers[0];
 		String strNum2 = twoNumbers[1];
 		String[] arabNums = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
@@ -26,25 +72,25 @@ public class Calculator {
 		}
 		catch (Exception e) {
 			if (strNum1.equals(romanNums[0])) {
-				num1 = 1;
+				num3 = 1;
 			} else if (strNum1.equals(romanNums[1])) {
-				num1 = 2;
+				num3 = 2;
 			} else if (strNum1.equals(romanNums[2])) {
-				num1 = 3;
+				num3 = 3;
 			} else if (strNum1.equals(romanNums[3])) {
-				num1 = 4;
+				num3 = 4;
 			} else if (strNum1.equals(romanNums[4])) {
-				num1 = 5;
+				num3 = 5;
 			} else if (strNum1.equals(romanNums[5])) {
-				num1 = 6;
+				num3 = 6;
 			} else if (strNum1.equals(romanNums[6])) {
-				num1 = 7;
+				num3 = 7;
 			} else if (strNum1.equals(romanNums[7])) {
-				num1 = 8;
+				num3 = 8;
 			} else if (strNum1.equals(romanNums[8])) {
-				num1 = 9;
+				num3 = 9;
 			} else if (strNum1.equals(romanNums[9])) {
-				num1 = 10;
+				num3 = 10;
 			} else {
 				System.out.println("Error, repeat operation1");
 			}
@@ -56,124 +102,97 @@ public class Calculator {
 		}
 		catch (Exception e) {
 			if (strNum2.equals(romanNums[0])) {
-				num2 = 1;
+				num4 = 1;
 			} else if (strNum2.equals(romanNums[1])) {
-				num2 = 2;
+				num4 = 2;
 			} else if (strNum2.equals(romanNums[2])) {
-				num2 = 3;
+				num4 = 3;
 			} else if (strNum2.equals(romanNums[3])) {
-				num2 = 4;
+				num4 = 4;
 			} else if (strNum2.equals(romanNums[4])) {
-				num2 = 5;
+				num4 = 5;
 			} else if (strNum2.equals(romanNums[5])) {
-				num2 = 6;
+				num4 = 6;
 			} else if (strNum2.equals(romanNums[6])) {
-				num2 = 7;
+				num4 = 7;
 			} else if (strNum2.equals(romanNums[7])) {
-				num2 = 8;
+				num4 = 8;
 			} else if (strNum2.equals(romanNums[8])) {
-				num2 = 9;
+				num4 = 9;
 			} else if (strNum2.equals(romanNums[9])) {
-				num2 = 10;
+				num4 = 10;
 			} else {
 				System.out.println("Error, repeat operation2");
 			}
 		}
 		
-//		if ((strNum1.equals(arabNums[0-9]) && strNum2.equals(romanNums[0-9])) ||  (strNum2.equals(arabNums[0-9]) && strNum1.equals(romanNums[0-9]))) {
-//			System.out.println("Error, you inputted arabic and roman digits");
-//		}
-//		
-//		if (strNum1.equals(arabNums[0]) || strNum1.equals(romanNums[0])) {
-//			num1 = 1;
-//		} else if (strNum1.equals(arabNums[1]) || strNum1.equals(romanNums[1])) {
-//			num1 = 2;
-//		} else if (strNum1.equals(arabNums[2]) || strNum1.equals(romanNums[2])) {
-//			num1 = 3;
-//		} else if (strNum1.equals(arabNums[3]) || strNum1.equals(romanNums[3])) {
-//			num1 = 4;
-//		} else if (strNum1.equals(arabNums[4]) || strNum1.equals(romanNums[4])) {
-//			num1 = 5;
-//		} else if (strNum1.equals(arabNums[5]) || strNum1.equals(romanNums[5])) {
-//			num1 = 6;
-//		} else if (strNum1.equals(arabNums[6]) || strNum1.equals(romanNums[6])) {
-//			num1 = 7;
-//		} else if (strNum1.equals(arabNums[7]) || strNum1.equals(romanNums[7])) {
-//			num1 = 8;
-//		} else if (strNum1.equals(arabNums[8]) || strNum1.equals(romanNums[8])) {
-//			num1 = 9;
-//		} else if (strNum1.equals(arabNums[9]) || strNum1.equals(romanNums[9])) {
-//			num1 = 10;
-//		} else {
-//			System.out.println("Error, repeat operation1");
-//		}
-//		
-//		System.out.println(num1);
-//		
-//		if (strNum2.equals(arabNums[0]) || strNum2.equals(romanNums[0])) {
-//			num2 = 1;
-//		} else if (strNum2.equals(arabNums[1]) || strNum2.equals(romanNums[1])) {
-//			num2= 2;
-//		} else if (strNum2.equals(arabNums[2]) || strNum2.equals(romanNums[2])) {
-//			num2 = 3;
-//		} else if (strNum2.equals(arabNums[3]) || strNum2.equals(romanNums[3])) {
-//			num2 = 4;
-//		} else if (strNum2.equals(arabNums[4]) || strNum2.equals(romanNums[4])) {
-//			num2 = 5;
-//		} else if (strNum2.equals(arabNums[5]) || strNum2.equals(romanNums[5])) {
-//			num2 = 6;
-//		} else if (strNum2.equals(arabNums[6]) || strNum2.equals(romanNums[6])) {
-//			num2 = 7;
-//		} else if (strNum2.equals(arabNums[7]) || strNum2.equals(romanNums[7])) {
-//			num2 = 8;
-//		} else if (strNum2.equals(arabNums[8]) || strNum2.equals(romanNums[8])) {
-//			num2 = 9;
-//		} else if (strNum2.equals(arabNums[9]) || strNum2.equals(romanNums[9])) {
-//			num2 = 10;
-//		} else {
-//			System.out.println("Error, repeat operation2");
-//		}
-//
-//		System.out.println(num2);
-//		char[] operationArray = toCalculate.toCharArray();
-//		int result = 0;
-//		
-//		for (int i = 1; i < operationArray.length; i++) {
-//			if (operationArray.equals(operation[0])) {
-//				result = num1+num2;
-//				System.out.println("Plus");
-//			} else if (operationArray.equals(operation[1])) {
-//				result = num1-num2;
-//				System.out.println(result);
-//			} else if (operationArray.equals(operation[2])) {
-//				result = num1*num2;
-//				System.out.println(result);
-//			} else if (operationArray.equals(operation[3])) {
-//				result = num1/num2;
-//				System.out.println(result);
-//			} else {
-//				System.out.println("Error, reapeat operation3");
-//			}
-//		}
 		
-//		if (toCalculate.contains("+") {
-//			System.out.println(num1+num2);
-//		}
-		if (toCalculate.indexOf("+") !=-1) {
-			System.out.println(num1+num2);
-//			System.out.println("+");
-		} else if (toCalculate.indexOf("-") !=-1) {
-			System.out.println(num1-num2);
-//			System.out.println("-");
-		} else if (toCalculate.indexOf("*") !=-1) {
-			System.out.println(num1*num2);
-//			System.out.println("*");
-		} else if (toCalculate.indexOf("/") !=-1) {
-			System.out.println(num1/num2);
-//			System.out.println("/");
+		
+		
+		int result1 = 0;
+		int result2 = 0;
+		String result3 = "";
+		
+		if (num1 != 0 && num2 != 0) {
+			if (toCalculate.indexOf("+") !=-1) {
+				result1 = num1+num2;
+//				System.out.println("+");
+			} else if (toCalculate.indexOf("-") !=-1) {
+				result1 = num1-num2;
+//				System.out.println("-");
+			} else if (toCalculate.indexOf("*") !=-1) {
+				result1 = num1*num2;
+//				System.out.println("*");
+			} else if (toCalculate.indexOf("/") !=-1) {
+				result1 = num1/num2;
+//				System.out.println("/");
+			} else {
+				System.out.println("Error, reapeat operation3");
+			}
+			System.out.println(result1);
+		} else if (num3 != 0 && num4 != 0){
+			if (toCalculate.indexOf("+") !=-1) {
+				result2 = num3+num4;
+//				System.out.println("+");
+			} else if (toCalculate.indexOf("-") !=-1) {
+				result2 = num3-num4;
+//				System.out.println("-");
+			} else if (toCalculate.indexOf("*") !=-1) {
+				result2 = num3*num4;
+//				System.out.println("*");
+			} else if (toCalculate.indexOf("/") !=-1) {
+				result2 = num3/num4;
+//				System.out.println("/");
+			} else {
+				System.out.println("Error, reapeat operation3");
+			}
+			result3 = IntegerToRomanNumeral(result2);
+			System.out.println(result3);
 		} else {
-			System.out.println("Error, reapeat operation3");
+			System.out.println("Error input data");
 		}
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
